0ba9bcc4caa489343fabc0d871dc5278e6ead892
aniket <aniketdeshmukh7798@gmail.com>
2023-11-01 16:32:41 +0530

    birth

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState,useEffect} from 'react';
+import './mobile.css';
+import Data from './data'
 
-function App() {
+const App = () => {
+  const[people,setPeople]=useState(Data)
+ 
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='bg'>
+     <div className='mob'> 
+     <div className='top'><h2>{people.length} Birthday's Today</h2></div>
+     {people.map((x)=>{
+        return(
+          <section className='list' key={x.id}>
+           <img src={x.image}></img>
+            <div className='name'><h3>{x.name}</h3>
+            <p>{x.age}</p> </div>
+          </section>
+        )
+      })}
+          
+      <button className='Button' onClick={()=>setPeople([])}>Clear</button></div>
     </div>
-  );
+  )
 }
-
-export default App;
+export default App
